741dc425d2518ef47bd728e31d95450f65d3e6da
wooddsy <wooddsy@gmail.com>
2021-09-14 14:39:18 +0100

    https://github.com/wooddsy/TAGOrbatManager/projects/1#card-68751046

---
+++ TAGOrbatManager/Models/Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TAGOrbatManager.Models
+{
+    public class Member
+    {
+        public Guid Guid { get; set; }
+        public string Name {  get; set; }
+        public string Role {  get; set; }
+        public DateTime JoinDate { get; set; }
+
+    }
+}
